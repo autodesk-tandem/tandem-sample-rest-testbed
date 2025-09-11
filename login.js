e7bb6ab6b69d1259372fde73c0fd9c7706491cfc
Jan Liska <jan.liska@autodesk.com>
2025-09-11 10:05:36 +0200

    updated auth flow to support PKCE

--- login.js
+++ login.js
@@ -9,9 +9,10 @@ const show = (id) => { getElem(id).style.display="block"};
 const hide = (id) => { getElem(id).style.display="none"};
 
 
-export function login() {
-  const scope = encodeURIComponent('data:read data:write user-profile:read');
-  doRedirection(env.forgeKey, scope);
+export async function login() {
+  const scope = 'data:read data:write user-profile:read';
+
+  await doRedirection(env.forgeKey, scope);
 }
 
 export function logout() {
@@ -20,34 +21,98 @@ export function logout() {
 };
 
   // when HTML page opens up, attach callbacks for login.logout and set values for current state UI
-export function checkLogin(idStr_login, idStr_logout, idStr_userProfile, idStr_viewer) {
+export async function checkLogin(idStr_login, idStr_logout, idStr_userProfile, idStr_viewer) {
 
-  getElem(idStr_login).addEventListener("click", login);
+  getElem(idStr_login).addEventListener("click", async () => {
+    await login();
+  });
   getElem(idStr_logout).addEventListener("click", logout);
+  const url = new URL(location);
 
-  if (!!location.hash)
-      setTokenStorage();
+  if (url.searchParams.has('code')) {
+    const code = url.searchParams.get('code');
+    const codeVerifier = window.localStorage.getItem('codeVerifier');
 
+    if (code && codeVerifier) {
+      try {
+        const payload = {
+          'grant_type': 'authorization_code',
+          'client_id': env.forgeKey,
+          'code_verifier': codeVerifier,
+          'code': code,
+          'redirect_uri': env.loginRedirect
+        };
+        const resp = await fetch('https://developer.api.autodesk.com/authentication/v2/token', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: Object.keys(payload).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(payload[key])).join('&')
+        });
+        
+        if (resp.ok) {
+          const credentials = await resp.json();
+        
+          window.sessionStorage.token = credentials['access_token'];
+        }
+      } catch (err) {
+        console.error(err);
+      }
+      // remove code from URL
+      url.searchParams.delete('code');
+      window.history.replaceState({}, document.title, url.pathname + url.search);
+    }
+  }
   if (window.sessionStorage.token) {
     hide(idStr_login);
     show(idStr_logout);
     show(idStr_userProfile);
     loadUserProfileImg(idStr_userProfile);
-    location.hash="";
-
     return true;  // they are logged in
   }
   else {
     hide(idStr_logout);
     hide(idStr_userProfile);
-
     return false; // they are not logged in
   }
 }
 
-export function doRedirection(forge_clientID, scope) {
-    const redirect_uri = encodeURIComponent(location.href.split('#')[0]);
-    location.href = `${env.forgeHost}/authentication/v2/authorize?response_type=token&client_id=${forge_clientID}&redirect_uri=${redirect_uri}&scope=${scope}`;
+function generateRandomString(length) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const array = new Uint8Array(length);
+  window.crypto.getRandomValues(array);
+  for (let i = 0; i < length; i++) {
+    result += chars[array[i] % chars.length];
+  }
+  return result;
+}
+
+async function generateCodeChallenge(str) {
+  const hash = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(str));
+  
+  return window.btoa(String.fromCharCode(...new Uint8Array(hash))).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+export async function doRedirection(forge_clientID, scope) {
+    const redirect_uri = env.loginRedirect;
+    const codeVerifier = generateRandomString(64);
+    const challenge = await generateCodeChallenge(codeVerifier);
+
+    window.localStorage.setItem('codeVerifier', codeVerifier);
+    const nonce='12345';
+    const url = new URL('https://developer.api.autodesk.com/authentication/v2/authorize');
+      
+    url.searchParams.append('response_type', 'code');
+    url.searchParams.append('client_id', forge_clientID);
+    url.searchParams.append('redirect_uri', redirect_uri);
+    url.searchParams.append('scope', scope);
+    //url.searchParams.append('nonce', nonce);
+    //url.searchParams.append('prompt', 'login');
+    url.searchParams.append('code_challenge', challenge);
+    url.searchParams.append('code_challenge_method', 'S256');
+
+    location.href = url.toString();
 }
 
 export function setTokenStorage() {
